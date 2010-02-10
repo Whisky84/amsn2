8239a4c53db5f27ddbcea499499f0a407093de4a
Gabriele "Whisky" Visconti <whisky.gabriele@gmail.com>
2010-02-10 22:30:17 +0000

    aMSNMainLoop implemented

--- amsn2/ui/front_ends/__init__.py
+++ amsn2/ui/front_ends/__init__.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
+
 import efl
 import cocoa
 import curses
 import gtk
 import qt4
 import web
-
+import kde4
--- amsn2/ui/front_ends/kde4/aMSNmain_loop/aMSNmain_loop.py
+++ amsn2/ui/front_ends/kde4/aMSNmain_loop/aMSNmain_loop.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+
 from amsn2.ui import base
+
+from PyKDE4.kdeui import *
+from PyKDE4.kdecore import *
+
+from PyQt4.QtCore import *
+from PyQt4.QtCore import SIGNAL
+import gobject
+
 
 class aMSNMainLoop(base.aMSNMainLoop):
     """ This Interface represents the main loop abstraction of the application.
@@ -9,13 +20,28 @@ class aMSNMainLoop(base.aMSNMainLoop):
         """
         @type amsn_core: aMSNCore
         """
+        print "\t\t\t\taMSNMainLoop.__init__()"
+        self._core = amsn_core
+        #Qt event loop crashes if we don't set this
+        os.putenv("QT_NO_GLIB","1") 
+        self.aboutData = KAboutData("amsn2", "", ki18n("aMSN2"), "0.001")
+        KCmdLineArgs.init(sys.argv[2:], self.aboutData)
+        self.app = KApplication()
+        #What are these for? :P
+        self.gmainloop = gobject.MainLoop()
+        self.gcontext = self.gmainloop.get_context()
 
-        print "NotImplementedError:\t\taMSNMainLoop.__init__()"
 
     def run(self):
         """ This will run the the main loop"""
-        print "NotImplementedError:\t\taMSNMainLoop.run()"
+        print "\t\t\t\taMSNMainLoop.run()"
+        self.idletimer = QTimer(KApplication.instance())
+        QObject.connect(self.idletimer, SIGNAL('timeout()'), self.qslot_on_idle)
+        self.idletimer.start(100)
+        self.app.exec_()
 
+    
+    #What are these methods for? 
     def idler_add(self, func):
         """
         This will add an idler function into the main loop's idler
@@ -23,6 +49,7 @@ class aMSNMainLoop(base.aMSNMainLoop):
         @type func: function
         """
         print "NotImplementedError:\t\taMSNMainLoop.idler_add()"
+        
 
     def timer_add(self, delay, func):
         """
@@ -32,8 +59,20 @@ class aMSNMainLoop(base.aMSNMainLoop):
         @type func: function
         """
         print "NotImplementedError:\t\taMSNMainLoop.timer_add()"
+        
 
     def quit(self):
         """ This will be called when the core wants to exit """
-        print "NotImplementedError:\t\taMSNMainLoop.quit()"
+        print "\t\t\t\taMSNMainLoop.quit()"
+        self.app.quit()
+        
+    
+    # -------------------- QT_SLOTS
+    
+    def qslot_on_idle(self):
+        iter = 0
+        while iter < 10 and self.gcontext.pending():
+            self.gcontext.iteration()
+            iter += 1
+            
 
