52a08d1a72a06112b5f8cafc69087f7e9a754155
Whisky <fastfading@mercurius.(none)>
2010-10-06 03:02:09 +0200

    kde4: completely implemented the Smiley selectort widget, with a nice rounded appearance, a nice fading effect when hiding, and a nice placment policy :)

--- amsn2/ui/front_ends/kde4/frontend/widgets/kfeEmoticonPopup.py
+++ amsn2/ui/front_ends/kde4/frontend/widgets/kfeEmoticonPopup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from amsn2.ui.front_ends.kde4.adaptationLayer import KFEThemeManager
+from amsn2.ui.front_ends.kde4.adaptationLayer import KFELog, KFEThemeManager
 from amsn2.core.smiley_manager  import aMSNSmileyManager
 from amsn2.core.theme_manager   import aMSNThemeManager
 
@@ -11,49 +11,63 @@ from PyQt4.QtCore   import *
 import math
 
 
-class KFEEmoticonPopup (QFrame):
+class KFEEmoticonPopup (QDockWidget):
+    fadeTime = 200
+    remainingFadeTime = fadeTime
     emoticonSelected = pyqtSignal("QString")
     def __init__(self, parent=None):
-        QWidget.__init__(self, parent, Qt.Popup)
+        QDockWidget.__init__(self, parent, Qt.Popup)
+        self.timer = QTimer(self)
+        self.timer.setSingleShot(True)
+        self.fadeTimer = QTimer(self)
+        self.fadeTimer.setSingleShot(False)
         themeManager = KFEThemeManager()
         # FIXME: Smiley manager's implementation will likely change heavily
         smileyDict = aMSNSmileyManager(None).default_smileys_shortcuts
-        smileyKActionList = []
+        smileyButtonList = []
     
         for i in smileyDict.keys():
             icon = KIcon(QIcon(themeManager.pathOf(smileyDict[i])))
-            action = KEmoticonAction(icon, "%s - %s" %(i, smileyDict[i]), i, self)
-            action.selected.connect(self.onEmoticonSelected)
-            smileyKActionList.append(action)
+            button = SmileyButton(icon, smileyDict[i],  i)
+            button.setFlat(True)
+            w, h = button.size().width(), button.size().height()
+            button.resize(min(w, h), min(w, h))
+            button.selected.connect(self.onEmoticonSelected)
+            smileyButtonList.append(button)
             
         #calculate dimensions:
-        # consraints: x*y = len(smileyDict) = r AND  x/y = 1.6
+        # constraints: x*y = len(smileyDict) = r AND  x/y = 1.6
         # which gives: y = sqrt(p/r) AND x = sqrt(r*p)
-        p = len(smileyKActionList)
-        y = math.ceil(math.sqrt(1.6 * p))
-        x = p / y
+        p = len(smileyButtonList)
+        self.y = math.ceil(math.sqrt(1.6 * p)) #numero di colonne
+        self.x = p / self.y #numero di righe
         
+        grid = QGridLayout()
+        grid.setSpacing(0)
         counter = 0
-        bars = []
-        for i in range(x):
-            bar = KToolBar(self)
-            for j in range(y):
+        for i in range(self.x):
+            for j in range(self.y):
                 if counter >= p:
                     continue
-                bar.addAction(smileyKActionList[counter])
+                grid.addWidget(smileyButtonList[counter], i,  j)
                 counter += 1
-            bar.setToolButtonStyle(Qt.ToolButtonIconOnly)
-            bar.setIconSize(QSize(16,16))
-            bars.append(bar)
-            
-        lay = QVBoxLayout()
-        for i in range(x):
-            lay.addWidget(bars[i])
-        self.setLayout(lay)
+
+        centralWidget = QWidget()
+        centralWidget.setLayout(grid)
         
-        self.setFrameShape(QFrame.StyledPanel)
-        self.setFrameShadow(QFrame.Raised)
-        #self.setWindowFlags(Qt.SubWindow)
+        self.setWidget(QWidget())
+        self.setFeatures(QDockWidget.NoDockWidgetFeatures)
+        self.setTitleBarWidget(centralWidget)
+        
+        self.timer.timeout.connect(self.fadeHide)
+        self.fadeTimer.timeout.connect(self.fadeHide)
+        
+    def enterEvent(self, event):
+        if self.timer.isActive():
+            self.timer.stop()
+        
+    def leaveEvent(self, event):
+        self.timer.start(750)
         
     def onEmoticonSelected(self, shortcut):
         self.hide()
@@ -61,17 +75,51 @@ class KFEEmoticonPopup (QFrame):
         
     def show(self):
         pos = QCursor.pos()
-        self.move(pos)
+        x = pos.x()
+        y = pos.y()
+        
+        screenGeometry = KApplication.kApplication().desktop().screenGeometry()
+        maxX = screenGeometry.width()
+        maxY = screenGeometry.height()
+        
+        mySize = self.sizeHint()
+        deltaX = mySize.width()
+        deltaY = mySize.height()
+        
+        if (x + deltaX) > maxX:
+            x = maxX - deltaX
+        if (y + deltaY) > maxY:
+            y = maxY - deltaY
+        
+        self.move(QPoint(x, y))
         QWidget.show(self)
+    
+    def fadeHide(self):
+        opacity = self.windowOpacity()
+        opacity -= 0.1
+        
+        if opacity > 0.2:
+            self.setWindowOpacity(self.windowOpacity() - 0.1)
+            if not self.fadeTimer.isActive():
+                self.fadeTimer.start(self.fadeTime / 9)
+        else:
+            self.hide()
+            self.setWindowOpacity(1)
+            self.fadeTimer.stop()
 
 
 
-class KEmoticonAction (KAction):
+
+class SmileyButton (KPushButton):
     selected = pyqtSignal("QString")
-    def __init__(self, icon, text, shortcut, parent):
-        KAction.__init__(self, icon, text, parent)
+    def __init__(self, icon, tooltip, shortcut,  parent=None):
+        KPushButton.__init__(self, icon, QString(), )
+        self.setToolTip("<b>%s</b> %s" % (shortcut, tooltip) )
         self.shortcut = QString(shortcut)
-        self.triggered.connect(self.__emitSignal)
+        self.clicked.connect(self.__emitSignal)
         
     def __emitSignal(self):
         self.selected.emit(self.shortcut)
+        
+    def sizeHint(self):
+        return QSize(25, 25)
